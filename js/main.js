e76f06cd2be9577c89191c67ad995f7ad5fdd60c
gadigr <gadigr@gmail.com>
2014-03-24 18:48:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-var constraints = {audio: false, video: true};
-var video = document.querySelector("video");
+  var constraints = {audio: false, video: true};
+  var video = document.querySelector('video');
   var canvas = document.querySelector('canvas');
   var ctx = canvas.getContext('2d');
   var localMediaStream = null;
 
   function snapshot() {
     if (localMediaStream) {
-      alert("a");
       ctx.drawImage(video, 0, 0);
       // "image/webp" works in Chrome.
       // Other browsers will fall back to image/png.
       document.querySelector('img').src = canvas.toDataURL('image/webp');
     }
   }
-  
+
   video.addEventListener('click', snapshot, false);
 
-function successCallback(stream) {
-  window.stream = stream; // stream available to console
-  if (window.URL) {
-    video.src = window.URL.createObjectURL(stream);
-  } else {
-    video.src = stream;
-  }
-  video.play();
-}
-
-function errorCallback(error){
-  console.log("navigator.getUserMedia error: ", error);
-}
-
-//navigator.getUserMedia(constraints, successCallback, errorCallback);
-// Not showing vendor prefixes or code that works cross-browser.
+  // Not showing vendor prefixes or code that works cross-browser.
   navigator.getUserMedia({video: true}, function(stream) {
     video.src = window.URL.createObjectURL(stream);
     localMediaStream = stream;
   }, errorCallback);
+  
+//function successCallback(stream) {
+//  window.stream = stream; // stream available to console
+//  if (window.URL) {
+//    video.src = window.URL.createObjectURL(stream);
+//  } else {
+//    video.src = stream;
+//  }
+//  video.play();
+//}
+
+//function errorCallback(error){
+//  console.log("navigator.getUserMedia error: ", error);
+//}
+
+//navigator.getUserMedia(constraints, successCallback, errorCallback);
