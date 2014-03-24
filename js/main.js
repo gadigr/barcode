7cd0960f7ba43779b271992f2bd154d3323153e1
gadigr <gadigr@gmail.com>
2014-03-25 01:07:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ video.addEventListener('loadeddata', function() {
  var result = getBarcodeFromImage(document.getElementById('barcode'));
  if(result != false)
 	 {
-	 	if(result.indexOf("X") == -1)
+	 	if(result.indexOf("2") != -1)
 	 	{
 		alert(result);
 	 	}
